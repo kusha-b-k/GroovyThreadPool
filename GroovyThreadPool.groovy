1f68c6929f30d5a58f02e11dc412430dfad052fc
kusha baledoni krishnamurthy <kushabaledonikrishnamurthy@1010011110011.local>
2015-12-25 13:02:48 +0530

    groovy kusha

---
+++ GroovyThreadPool.groovy
@@ -0,0 +1,28 @@
+/**
+ * Created by kusha bk
+ */
+
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
+
+
+
+def mThread = { mNumber -> println "pool GRooVY PooL ${ mNumber }"}
+
+
+def mPoolThread = Executors.newFixedThreadPool(3)
+try {
+            def futures = (0..100).collect{ mNumber->
+
+            mPoolThread.submit({->
+                sleep(100)
+            mThread mNumber } as Callable);
+
+    }
+    futures.each{ it.get() }
+
+}
+finally
+        {
+            mPoolThread.shutdown()
+        }
